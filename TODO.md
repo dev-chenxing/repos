798515c761bacfac330617d1b280a42b676be769
dev-chenxing <chenxing.io@outlook.com>
2024-11-12 14:30:22 +0800

    multi langs

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
 # TODO
 
 - [ ] Topics
+- [ ] Skip no languages repos
--- index.php
+++ index.php
@@ -1,34 +1,38 @@
 <?php
 
-$curl_handle = curl_init();
-curl_setopt($curl_handle, CURLOPT_URL, "https://api.github.com/search/repositories?q=+stars:12..24+sort:updated&order:desc");
-curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, value: true);
-curl_setopt($curl_handle, CURLOPT_HTTPHEADER, [
-    "Accept: application/vnd.github.v3+json",
-    "User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Mobile Safari/537.36",
-]);
+function curl(string $url): array|bool
+{
+    $curl_handle = curl_init();
+    curl_setopt($curl_handle, CURLOPT_URL, $url);
+    curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, value: true);
+    curl_setopt($curl_handle, CURLOPT_HTTPHEADER, [
+        "Accept: application/vnd.github.v3+json",
+        "User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Mobile Safari/537.36",
+    ]);
 
-$response = curl_exec($curl_handle);
-curl_close($curl_handle);
-
-if ($response === false) {
-    die(curl_error($curl_handle));
+    $response = curl_exec($curl_handle);
+    curl_close($curl_handle);
+    if ($response === false) {
+        die(curl_error($curl_handle));
+    }
+    return json_decode($response, true);
 }
 
-$data = json_decode($response, true);
-// var_dump($data);
+$data = curl("https://api.github.com/search/repositories?q=+stars:12..24+sort:updated&order:desc");
 
 $json_path = "repos.json";
 $Repos = json_decode(file_get_contents($json_path), true);
 
 foreach ($data["items"] as $repo) {
-    $Repos[$repo["id"]] = [
-        "name" => $repo["full_name"],
-        "url" => $repo["html_url"],
-        "desc" => $repo["description"],
-        "lang" => $repo["language"],
-        "update" => $repo["updated_at"]
-    ];
+    if ($repo["language"]) {
+        $Repos[$repo["id"]] = [
+            "name" => $repo["full_name"],
+            "url" => $repo["html_url"],
+            "desc" => $repo["description"],
+            "langs" => implode(", ", array_keys(curl($repo["languages_url"]))),
+            "update" => $repo["updated_at"]
+        ];
+    }
 }
 
 function sort_repos($a, $b)
@@ -43,7 +47,7 @@ function generateReadme()
     $readme .= "| Language | Repo | Description |\n";
     $readme .= "| - | --------- | --------- |\n";
     foreach ($GLOBALS["Repos"] as $Repo) {
-        $readme .= "| " . $Repo["lang"] . " | [" . $Repo["name"] . "](" . $Repo["url"] . ") | " . $Repo["desc"] . " |\n";
+        $readme .= "| " . $Repo["langs"] . " | [" . $Repo["name"] . "](" . $Repo["url"] . ") | " . $Repo["desc"] . " |\n";
     }
     return $readme;
 }
